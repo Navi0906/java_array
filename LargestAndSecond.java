46f61ab3b9330ace9b47b2e7ef75cb5be0735058
Navi0906 <naam0906.be21@chitkara.edu.in>
2025-03-04 14:42:17 +0530

    Naam Simran Singh Add: Finding largest and second largest digit

---
+++ LargestAndSecond.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class LargestAndSecond {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        // Extract digits from the number and store them in the array
+        while (number != 0) {
+            int digit = number % 10;
+            digits[index] = digit;
+            index++;
+            number /= 10;
+
+            // Check if we need to increase the size of the array
+            if (index == maxDigit) {
+                maxDigit += 10;
+                int[] temp = new int[maxDigit];
+                for (int i = 0; i < digits.length; i++) {
+                    temp[i] = digits[i];
+                }
+                // Assign the new array to digits
+                digits = temp;
+            }
+        }
+
+        int largest = -1; 
+        int secondLargest = -1;
+
+        // Loop through the array to find the largest and second largest digits
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest; 
+                largest = digits[i]; 
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i]; 
+            }
+        }
+
+        System.out.println("Largest digit: " + largest);
+        System.out.println("Second largest digit: " + secondLargest);
+    }
+}
